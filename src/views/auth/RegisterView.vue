7c87e2007112d2ea983db265a2f6e6c779a5fac8
cjdumago <cjdumago7@gmail.com>
2025-04-03 13:25:22 +0800

    feat: added login and register page

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue'
 
 const theme = ref('light')
+const showPassword = ref(false) // Ref to toggle password visibility
+const showPasswordConfirmation = ref(false) // Ref for confirmation field
 
 function onClick() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
@@ -28,8 +30,8 @@ function onClick() {
             <v-col cols="12" md="6" class="mx-auto">
               <v-card
                 class="mx-auto mt-16"
-                prepend-icon="mdi-login"
-                subtitle="Login"
+                prepend-icon="mdi-account-plus"
+                subtitle="Register"
                 image="/images/bg-card.jpg"
                 elevation="24"
               >
@@ -39,22 +41,35 @@ function onClick() {
 
                 <v-card-text class="bg-surface-light pt-4">
                   <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Firstname" variant="outlined"></v-text-field>
+
+                    <v-text-field label="Lastname" variant="outlined"></v-text-field>
+
                     <v-text-field label="Email" variant="outlined"></v-text-field>
 
                     <v-text-field
                       label="Password"
-                      type="password"
+                      :type="showPassword ? 'text' : 'password'"
                       variant="outlined"
+                      :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                      @click:append="showPassword = !showPassword"
+                    ></v-text-field>
+
+                    <v-text-field
+                      label="Password Confirmation"
+                      :type="showPasswordConfirmation ? 'text' : 'password'"
+                      variant="outlined"
+                      :append-icon="showPasswordConfirmation ? 'mdi-eye-off' : 'mdi-eye'"
+                      @click:append="showPasswordConfirmation = !showPasswordConfirmation"
                     ></v-text-field>
 
                     <v-btn class="mt-2" type="submit" block>Submit</v-btn>
                   </v-form>
 
-                  <v-divider class="my-5"> </v-divider>
+                  <v-divider class="my-5"></v-divider>
 
                   <h5 class="text-center">
-                    Don't have Account?
-                    <RouterLink to="/register">Click here to register</RouterLink>
+                    Already have account? <RouterLink to="/">Click here to Login</RouterLink>
                   </h5>
                 </v-card-text>
               </v-card>
