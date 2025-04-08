c1b8034612c6284fb6a243c14674dd70708178f9
Carlito G. Dumago Jr. <carlito.dumago@carsu.edu.ph>
2025-04-08 21:21:51 +0800

    Update LoginView.vue

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,44 +1,180 @@
 <script setup>
+import { ref } from 'vue'
 import AppLayout from '@/components/layout/AppLayout.vue'
-import LoginForm from '@/components/auth/LoginForm.vue'
-import { useDisplay } from 'vuetify'
 
-const { mobile } = useDisplay()
+const step = ref(1)
 </script>
 
 <template>
   <AppLayout>
     <template #content>
-      <v-row>
-        <v-col cols="12" md="6" class="mx-auto">
-          <v-card class="mx-auto mt-16" elevation="24">
-            <v-card-title class="text-center">
-              <v-img
-                class="mx-auto rounded-circle"
-                src="/images/logocq.jpg"
-                :width="mobile ? '75%' : '25%'"
-              ></v-img>
-              <h3 class="font-weight-black">Campus Quest</h3>
-              <p>Login Form</p>
-            </v-card-title>
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="10">
+            <v-card class="elevation-6 mt-10">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="6">
+                      <v-card-text class="mt-2">
+                        <v-img class="mx-auto" width="30%" src="/images/clogo.png"></v-img>
+                        <h4 class="text-center">Login in to Your Account</h4>
 
-            <v-card-text class="bg-surface-light pt-4">
-              <v-divider class="my-5"></v-divider>
+                        <v-row align="center" justify="center">
+                          <v-col cols="12" sm="8">
+                            <v-text-field
+                              label="Email"
+                              outlined
+                              dense
+                              color="blue"
+                              autocomplete="false"
+                              class="mt-16"
+                            />
+                            <v-text-field
+                              label="Password"
+                              outlined
+                              dense
+                              color="blue"
+                              autocomplete="false"
+                              type="password"
+                            />
+                            <v-row>
+                              <v-col cols="12" sm="7">
+                                <v-checkbox label="Remember Me" class="mt-n1" color="blue">
+                                </v-checkbox>
+                              </v-col>
+                              <v-col cols="12" sm="5">
+                                <span class="caption blue--text">Forgot password</span>
+                              </v-col>
+                            </v-row>
+                            <v-btn color="blue" dark block tile>Log in</v-btn>
 
-              <LoginForm></LoginForm>
+                            <h5 class="text-center grey--text mt-4 mb-3">Or Log in using</h5>
+                            <div class="d-flex justify-space-between align-center mx-10 mb-16">
+                              <v-btn depressed outlined color="grey">
+                                <v-icon color="red">mdi-google</v-icon>
+                              </v-btn>
+                              <v-btn depressed outlined color="grey">
+                                <v-icon color="blue">mdi-facebook</v-icon>
+                              </v-btn>
+                              <v-btn depressed outlined color="grey">
+                                <v-icon color="light-blue lighten-3">mdi-twitter</v-icon>
+                              </v-btn>
+                            </div>
+                          </v-col>
+                        </v-row>
+                      </v-card-text>
+                    </v-col>
+                    <v-col cols="12" md="6" class="blue rounded-bl-xl">
+                      <div style="text-align: center; padding: 180px 0">
+                        <v-card-text class="white--text">
+                          <h3 class="text-center">Don't Have an Account Yet?</h3>
+                          <h6 class="text-center">
+                            Let's get you all set up so you can start creating your your first<br />
+                            onboarding experience
+                          </h6>
+                        </v-card-text>
+                        <div class="text-center">
+                          <v-btn elevation="5" tile outlined dark @click="step++">SIGN UP</v-btn>
+                        </div>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row>
+                    <v-col cols="12" md="6" class="blue rounded-br-xl">
+                      <div style="text-align: center; padding: 180px 0">
+                        <v-card-text class="white--text">
+                          <h3 class="text-center">Alredy Signed up?</h3>
+                          <h6 class="text-center">
+                            Log in to your account so you can continue building and<br />
+                            editing your onboarding flows
+                          </h6>
+                        </v-card-text>
+                        <div class="text-center">
+                          <v-btn elevation="5" tile outlined dark @click="step--">Log in</v-btn>
+                        </div>
+                      </div>
+                    </v-col>
 
-              <v-divider class="my-5"> </v-divider>
+                    <v-col cols="12" md="6">
+                      <v-card-text class="mt-6">
+                        <v-img class="mx-auto" width="30%" src="/images/clogo.png"></v-img>
+                        <h4 class="text-center">Sign Up for an Account</h4>
+                        <v-row align="center" justify="center">
+                          <v-col cols="12" sm="8">
+                            <v-row>
+                              <v-col cols="12" sm="6">
+                                <v-text-field
+                                  label="First Name"
+                                  outlined
+                                  dense
+                                  color="blue"
+                                  autocomplete="false"
+                                  class="mt-4"
+                                />
+                              </v-col>
+                              <v-col cols="12" sm="6">
+                                <v-text-field
+                                  label="Last Name"
+                                  outlined
+                                  dense
+                                  color="blue"
+                                  autocomplete="false"
+                                  class="mt-4"
+                                />
+                              </v-col>
+                            </v-row>
+                            <v-text-field
+                              label="Email"
+                              outlined
+                              dense
+                              color="blue"
+                              autocomplete="false"
+                            />
+                            <v-text-field
+                              label="Password"
+                              outlined
+                              dense
+                              color="blue"
+                              autocomplete="false"
+                              type="password"
+                            />
+                            <v-row>
+                              <v-col cols="12" sm="7">
+                                <v-checkbox label="I Accept AAE" class="mt-n1" color="blue">
+                                </v-checkbox>
+                              </v-col>
+                              <v-col cols="12" sm="5">
+                                <span class="caption blue--text ml-n4">Terms &Conditions</span>
+                              </v-col>
+                            </v-row>
+                            <v-btn color="blue" dark block tile>Sign up</v-btn>
 
-              <h5 class="text-center">
-                Don't have Account?
-                <RouterLink class="text-deep-orange-lighten-1 font-weight-black" to="/register"
-                  >Click here to register</RouterLink
-                >
-              </h5>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+                            <h5 class="text-center grey--text mt-4 mb-3">Or Sign up using</h5>
+                            <div class="d-flex justify-space-between align-center mx-10 mb-11">
+                              <v-btn depressed outlined color="grey">
+                                <v-icon color="red">mdi-google</v-icon>
+                              </v-btn>
+                              <v-btn depressed outlined color="grey">
+                                <v-icon color="blue">mdi-facebook</v-icon>
+                              </v-btn>
+                              <v-btn depressed outlined color="grey">
+                                <v-icon color="light-blue lighten-3">mdi-twitter</v-icon>
+                              </v-btn>
+                            </div>
+                          </v-col>
+                        </v-row>
+                      </v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </template>
   </AppLayout>
 </template>
